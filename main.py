5e01dea7aeab5b21694e1e2b2d8c0c08778de906
Rithy <huotrithy01@gmail.com>
2023-07-05 22:45:06 +0700

    send

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import kivy
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-
 import pafy
 import vlc
 import googleapiclient.discovery
@@ -27,84 +26,20 @@ class Music(MDBoxLayout):
         if self.url[:20] != "https://youtube.com/":
             return "Invalid URL"
         
+        print(self.url)
+
         # Retrive ID from URL
         self.id = check_video_or_playlist(self.url)
+
+        print(self.id)
 
         # Validating
         if not self.id:
             return
 
         if self.id["type"] == "video":
-            api_key = "AIzaSyBkpY4xMyCSMgFX4opcnyS7Q03x96yzDFk"
-            # youtube = build("youtube", "v3", developerKey=api_key)
-
-            api_service_name = "youtube"
-            api_version = "v3"
-
-            youtube = googleapiclient.discovery.build(
-                api_service_name, api_version, developerKey = api_key)
-
-            playlist_id = "PLSR9lWowvoE3A9i4JVVHtQFjlJt0_LItG"
-            request = youtube.playlistItems().list(
-                part="snippet",
-                maxResults=50,
-                playlistId="PLSR9lWowvoE3A9i4JVVHtQFjlJt0_LItG"
-            )
-
-            playlist_item = request.execute()
-
-            # print(playlist_item)
-
-            video_link = []
-            for item in playlist_item["items"]:
-                video = {
-                    "video_id": item["snippet"]["resourceId"]["videoId"],
-                    "title": item["snippet"]["title"]
-                }
-                video_link.append(video)
-                
-
-            is_opening = False
-            is_playing = False
-
-            video_url = "https://www.youtube.com/watch?v=l2l-8jKsh8k&list=RDl2l-8jKsh8k&start_radio=1"
-            is_opening = False
-            is_playing = False
-
-
-            # Create new pafy object
-            video = pafy.new(video_url)
-
-            # extract best audio stream from youtube
-            audio_stream = video.getbestaudio()
-
-            # retrive audio stream url
-            audio_url = audio_stream.url
-
-            player = vlc.MediaPlayer()
-            media = vlc.Media(audio_url)
-            player.set_media(media)
-            player.play()
-
-
-
-            good_states = [
-                "State.Playing", 
-                "State.NothingSpecial", 
-                "State.Opening"
-            ]
-
-            while str(player.get_state()) in good_states:
-                if str(player.get_state()) == "State.Opening" and is_opening is False:
-                    print("Status: Loading")
-                    is_opening = True
-
-                if str(player.get_state()) == "State.Playing" and is_playing is False:
-                    print("Status: Playing")
-                    is_playing = True
-
-            print("Status: Finish")
-            player.stop()
+            pass
+        
 
 # playlist pattern https://youtube.com/playlist?list=PLOxu-EtycI1lOoGBpE508PuvBsGHLOihJ
 # video pattern https://www.youtube.com/watch?v=y6cWxCFx1i8&list=PLOxu-EtycI1lOoGBpE508PuvBsGHLOihJ&index=1
